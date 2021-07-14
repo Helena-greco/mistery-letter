bda7d9c1d890b5dd0ddbcc29e090cf8f27ed9374
Helena <nenafgreco@hotmail.com>
2021-07-14 15:30:22 -0300

    Requisito 3, 5 e 16

--- script.js
+++ script.js
@@ -7,15 +7,34 @@ function createLetter() {
   const letterText = inputText.value;
   const letterWords = letterText.split(' ');
   if (letterWords[0] === '') {
-    generateLetter.innerText = 'Por favor, digite o conteúdo da carta';
+    generateLetter.innerText = 'Por favor, digite o conteúdo da carta.';
+    generateLetter.style.fontWeight = '900';
   } else {
     for (let index = 0; index < letterWords.length; index += 1) {
       const spanWord = document.createElement('span');
-      const space = ' ';
-      spanWord.innerText = letterWords[index] + space;
+      spanWord.innerText = letterWords[index];
       generateLetter.appendChild(spanWord);
     }
   }
 }
 
 buttonLetter.addEventListener('click', createLetter);
+
+function generateClass() {
+  const span = document.querySelectorAll('span');
+  const estilo = ['newspaper', 'magazine1', 'magazine2'];
+  const tamanho = ['medium', 'big', 'reallybig'];
+  const rotacao = ['rotateleft', 'rotateright'];
+  const inclinacao = ['skewleft', 'skewright'];
+  for (let index = 0; index < span.length; index += 1) {
+    span[index].classList.add(estilo[Math.floor(Math.random() * 3)]);
+    span[index].classList.add(tamanho[Math.floor(Math.random() * 3)]);
+    span[index].classList.add(rotacao[Math.floor(Math.random() * 2)]);
+    span[index].classList.add(inclinacao[Math.floor(Math.random() * 2)]);
+  }
+  console.log(estilo[Math.floor(Math.random() * 3)]);
+}
+
+generateClass();
+
+buttonLetter.addEventListener('click', generateClass);
